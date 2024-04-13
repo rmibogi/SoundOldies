b0c07555681af1d1a2fda3dade46ffd932c5d2a2
Ruslan Agliullin <124513940+rmibogi@users.noreply.github.com>
2024-04-13 07:34:37 +0400

    added_sliders

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -182,8 +182,8 @@ bool SoundOldiesAudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* SoundOldiesAudioProcessor::createEditor()
 {
-    //return new SoundOldiesAudioProcessorEditor (*this);
-    return new juce::GenericAudioProcessorEditor(*this);
+    return new SoundOldiesAudioProcessorEditor (*this);
+    //return new juce::GenericAudioProcessorEditor(*this);
 }
 
 //==============================================================================
@@ -192,12 +192,21 @@ void SoundOldiesAudioProcessor::getStateInformation (juce::MemoryBlock& destData
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
+
+    juce::MemoryOutputStream mos(destData, true);
+    apvts.state.writeToStream(mos);
 }
 
 void SoundOldiesAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
+    auto tree = juce::ValueTree::readFromData(data, sizeInBytes);
+    if (tree.isValid())
+    {
+        apvts.replaceState(tree);
+        updateFilters();
+    }
 }
 
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts)
